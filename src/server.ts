ed346e691c00bdd5be312c334cfb333c65cdcf24
NarioMendonca <nario.sousa2014@gmail.com>
2026-07-30 18:12:32 -0300

    chore: configure fastify

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,36 @@
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import ScalarApiReference from "@scalar/fastify-api-reference";
 import { fastify } from "fastify";
+import {
+	jsonSchemaTransform,
+	serializerCompiler,
+	validatorCompiler,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-const app = fastify();
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+	origin: process.env.CLIENTS_URL?.split(",") ?? "http://localhost:8080",
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "RFID Manager Park API",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+	routePrefix: "/docs",
+});
 
 app.get("/", async () => {
 	return "Server running!";
@@ -9,4 +39,5 @@ app.get("/", async () => {
 const PORT = 3333;
 app.listen({ port: PORT, host: "0.0.0.0" }).then(() => {
 	console.log(`Server running on http://localhost:${PORT}`);
+	console.log(`Docs in http://localhost:${PORT}/docs`);
 });
