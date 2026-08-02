383c024283a0cce7799fafe6fcbb8cb6696e118e
NarioMendonca <nario.sousa2014@gmail.com>
2026-08-02 20:46:55 -0300

    fix: register missing routes

--- src/routes/menu-items/menu-items-routes.ts
+++ src/routes/menu-items/menu-items-routes.ts
@@ -1,8 +1,12 @@
 import type { FastifyInstance } from "fastify";
 import { createMenuItemRoute } from "./create-menu-item.route.js";
+import { deleteMenuItemRoute } from "./delete-menu-item.route.js";
 import { fetchMenuItemsRoute } from "./fetch-menu-items.route.js";
+import { updateMenuItemRoute } from "./update-menu-item.route.js";
 
 export async function menuItemsRoutes(app: FastifyInstance) {
 	app.register(fetchMenuItemsRoute);
 	app.register(createMenuItemRoute);
+	app.register(updateMenuItemRoute);
+	app.register(deleteMenuItemRoute);
 }
